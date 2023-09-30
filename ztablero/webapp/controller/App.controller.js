39fb70ff4061e4c07db5f69c202b11c46c3f8fec
Pavel Principex <pprincipe.cons@outlook.com>
2023-09-30 00:20:00 +0000

    first commit

---
+++ ztablero/webapp/controller/App.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define(
+    [
+        "sap/ui/core/mvc/Controller"
+    ],
+    function(BaseController) {
+      "use strict";
+  
+      return BaseController.extend("nebula.com.ztablero.controller.App", {
+        onInit() {
+        }
+      });
+    }
+  );
+  
---
+++ ztablero/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"nebulacom/ztablero/test/unit/controller/Main.controller"
+], function () {
+	"use strict";
+});
